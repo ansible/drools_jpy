f4d155bbc1523fa905ffe5f9df738ebdcb73a612
Madhu Kanoor <mkanoor@redhat.com>
2023-01-13 09:23:17 -0500

    AST Spec changes for once_within and timeout
    - Added throttle examples
    - Added once_after/once_within examples

--- src/drools/dispatch.py
+++ src/drools/dispatch.py
@@ -1,8 +1,11 @@
 import asyncio
 import json
+import logging
 from dataclasses import dataclass
 
 from .ruleset import RulesetCollection
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -11,11 +14,13 @@ class Dispatch:
     serialized_result: str
 
     def run(self) -> None:
+        logger.debug("Dispatching for session " + str(self.session_id))
         rs = RulesetCollection.get_by_session_id(self.session_id)
         rs.dispatch(self.serialized_result)
 
 
 async def establish_async_channel():
+    logger.debug("Establishing async channel")
     return await asyncio.open_connection(
         "localhost", RulesetCollection.response_port()
     )
@@ -26,8 +31,12 @@ async def handle_async_messages(reader, writer):
         while True:
             length = await reader.read(4)
             bytes_to_read = int.from_bytes(length, "big")
+            logger.debug(
+                "Reading " + str(bytes_to_read) + " from async channel"
+            )
             payload = await reader.read(bytes_to_read)
             if payload:
+                logger.debug("Async Response " + str(payload))
                 data = json.loads(payload)
                 for result in data["result"]:
                     dispatch = Dispatch(
@@ -36,6 +45,7 @@ async def handle_async_messages(reader, writer):
                     )
                     dispatch.run()
     except asyncio.CancelledError:
+        logger.debug("Shutting down async channel")
         RulesetCollection.shutdown()
     finally:
         writer.close()
